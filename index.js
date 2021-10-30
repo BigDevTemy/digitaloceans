b9dbdf8fb7a583fbc84ddd1ed745d7f3eb0b5cfa
Andre <quadhay@gmail.com>
2021-10-30 11:57:23 +0100

    Saved

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ import express from "express";
 import bodyParser from 'body-parser';
 import useRouter from './Route/users.js'
 import mongoose from "mongoose";
+import dotenv  from "dotenv"
+
+dotenv.config()
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 app.use('/',useRouter);
-mongoose.connect(
-    "mongodb+srv://admin:Ademilola@cluster0.9ymuh.mongodb.net/jupit_db?retryWrites=true&w=majority"
-)
+// "mongodb+srv://admin:Ademilola@cluster0.9ymuh.mongodb.net/jupit_db?retryWrites=true&w=majority"
+mongoose.connect(process.env.DATABASE_URI,{
+    tls:true,
+    tlsCAFile:'./ca-certificate.crt'
+});
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
